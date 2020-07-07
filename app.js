0861e1f37b07fa715bb52e19abb646b25226e8ec
Himani Popli <himaniheenu@gmail.com>
2020-07-08 00:13:21 +0530

    msg from server at regular interval

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var SocketServer = require('ws').Server;
 var wss = new SocketServer({ port:process.env.PORT||3000 });
 wss.on('connection', function connection(ws) {
+    setInterval(function () {
+        ws.send("Hello");
+    }, 2000);
     ws.on('message', function (message) {
         console.log("Received: " + message);
         ws.send("From Server: " + message);
